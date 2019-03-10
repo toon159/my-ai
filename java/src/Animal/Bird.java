6dff3d9adc874e0d0290d14fadd5d7baecf88fa5
Vichaya Raksakunpanich <toonbcc@gmail.com>
2019-03-10 13:49:45 +0700

    Static polymorphism

--- java/src/Animal/Bird.java
+++ java/src/Animal/Bird.java
@@ -11,4 +11,16 @@ public class Bird {
     public void flyDown() {
         System.out.println("Flying down...");
     }
+
+    public void fly() {
+        System.out.println("The bird is flying.");
+    }
+
+    public void fly(int height) {
+        System.out.println("The bird is flying " + height + " feet high.");
+    }
+
+    public void fly(String name, int height) {
+        System.out.println("The " + name + " is flying " + height + " feet high.");
+    }
 }
